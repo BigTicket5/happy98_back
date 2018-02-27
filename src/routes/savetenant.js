90b948c70580e6ebbe381388ff408b74dcd3745c
Zhenzhong Mao <dkrose.random@gmail.com>
2018-02-27 23:52:35 +0800

    update 20180227

--- src/routes/savetenant.js
+++ src/routes/savetenant.js
@@ -3,7 +3,7 @@ import TenantInit from "../models/TenantInit";
 const router =  express.Router();
 router.post("/",(req,res)=>{
     var newTenant = req.body.tenant;
-	TenantInit.create(newTenant,function(err,newTenant){
+	TenantInit.updateOne(newTenant,function(err,newTenant){
 		if(err){
 			res.status(400).json({errors:{global:"Invalid credentials"}});
 		}
